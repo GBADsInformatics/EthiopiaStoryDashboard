62e9e3dd5e8079ef61369523ea37cc5e22afa7ee
skytruong <yohskytruong@gmail.com>
2022-08-03 16:45:50 -0400

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+## Ethiopia Story Dashboard
--- ethiopia_story_dashboard.py
+++ ethiopia_story_dashboard.py
@@ -1,221 +1,171 @@
 # Ethiopia Story Dashboard
-# Date of last revision: July 26, 2022
+# Date of last revision: July 28, 2022
 
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc
-from dash.exceptions import PreventUpdate 
-import pandas as pd
-import numpy as np
-import json
+from dash.exceptions import PreventUpdate
 from dash import dash_table
 from dash_extensions.enrich import DashProxy, Output, Input, State, ServersideOutput, html, dcc, ServersideOutputTransform, FileSystemStore
-
+import pandas as pd
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+fss = FileSystemStore(cache_dir='ethdash_cache')
+app = DashProxy(transforms=[ServersideOutputTransform(backend=fss)], external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
-#  IMAGES
-# Example images set for the dashboard template, used for logos of the company/entity that is
-# showcasing the data visualization. Add more by adding local files to \assets or by image URL.
+# GBADS LOGO #
 GBADSLOGOB = "https://i0.wp.com/animalhealthmetrics.org/wp-content/uploads/2019/10/GBADs-LOGO-Black-sm.png"
 GBADSLOGOW = "https://i0.wp.com/animalhealthmetrics.org/wp-content/uploads/2019/10/GBADs-LOGO-White-sm.png"
 
+# VARIABLES DECLARATION #
 
-# TAB STYLING
-# This is the styling that is applied to the selected tab.
-selectedTabStyle = {
-    'border': '3px solid white',
-    'backgroundColor': 'white',
-    'color': 'black'
+# TAB STYLING #
+## Could not resolve this in CSS for some reason ##
+activeLabelStyle = {
+    'backgroundColor': '#C4C4C4',
+    'color': 'black',
 }
 
-# ******************* LAYOUT *******************
+# PAGE LAYOUT #
 app.layout = html.Div([
     
-    html.Div([
-        html.Div([html.Img(src=GBADSLOGOW, className="top")], style={'display': 'inline-block'}),
-        html.Div([html.H1('Ethiopia Case Study')], style={'display': 'inline-block', 'margin-left': '35%'}),
-    ]),
-    
-    html.Div([
+    # === PAGE HEADER === #
+    html.Div(
+        id='page-header',
+        children=[
+            html.Div([html.Img(src=GBADSLOGOW, className="top")], id='logo-img'),
+            html.Div([html.Div('Ethiopia Case Study')], id='dashboard-title'),
+        ]
+    ),
 
-        # ================= LEFT DIV ================= #
-        # This div includes all the components for the sidebar menu.
-        html.Div(
-            id="left-div",
-            children=[
-                html.Div([
-                    html.Div([
-
-                        # ====== SPECIES ====== #
-                        html.Div([
-                                # --- PANEL HEADING ---
-                                html.Div('Species', className='panel-heading'),
-                                html.Hr(style={"width": "90%"}),
-
-                                # --- OPTIONS ---
-                                html.Div([
-                                    html.Button('Cattle', id='cattle-report-btn', className='metadata-button')
-                                ], className='metadata-btn-container jc-center'),
-
-                                html.Div([
-                                    html.Button('Camels', id='camel-report-btn', className='metadata-button'),
-                                ], className='metadata-btn-container jc-center'),
-
-                                html.Div([
-                                    html.Button('Goats', id='goat-report-btn', className='metadata-button'),
-                                ], className='metadata-btn-container jc-center'),
-
-                                html.Div([
-                                    html.Button('Sheep', id='sheep-report-btn', className='metadata-button'),
-                                ], className='metadata-btn-container jc-center'),
-
-                            ], className='dropdown-t'),
-
-                        # ====== SPECIES OPTIONS ====== #
-                        # html.Div([
-
-                        #     html.Div([
-                        #         html.Button('Cattle', id='cattle-report-btn', className='metadata-button')
-                        #     ], className='metadata-btn-container jc-center'),
-
-                        #     html.Div([
-                        #         html.Button('Camels', id='camel-report-btn', className='metadata-button'),
-                        #     ], className='metadata-btn-container jc-center'),
-
-                        #     html.Div([
-                        #         html.Button('Goats', id='goat-report-btn', className='metadata-button'),
-                        #     ], className='metadata-btn-container jc-center'),
-
-                        #     html.Div([
-                        #         html.Button('Sheep', id='sheep-report-btn', className='metadata-button'),
-                        #     ], className='metadata-btn-container jc-center'),
-
-                        # ], className='dropdown-b'),
-
-                    ], className='dropdown-container'),
-
-                    # --- SIDEBAR ---
-                    html.Div ([
-                        # dbc.Button(children="<", id="left-button", className='buttonstyle'),
-                        dbc.Button(
-                            children=[
-                                html.I(className="fa-solid fa-angle-left")
-                            ],
-                            id='left-button', className='buttonstyle'
-                        )
-                    ], className="slider"),
-
-                ], className='left-container'),
-            ], 
-            
-            className="options-left"),
-
-            # ================= RIGHT DIV ================= #
-            html.Div(
-                id="right-div",
-                children=[
-                    html.Div([
-
-                            dcc.Tabs(
-                                id='tabs',
-                                children=[
-                                
-                                # ====== REPORTS ====== #
-                                dcc.Tab(
-                                    label='Reports', 
-                                    children=[
-                                        html.Div([
-                                            html.Div([
-                                                html.H3('Report portion', style={'color':'black'}),
-                                                # dcc.Loading(
-                                                #     id='faostat-graph-loading-2',
-                                                #     children=[dcc.Graph(id='faostat-graph-2', className='graph-size')],
-                                                #     type='circle'
-                                                # )
-                                            ]),
-                                        ], className="f-h-scroll-div"),
-                                    ],  
-                                    className='cattabs',
-                                    selected_style=selectedTabStyle
-                                ),
-
-                                # ====== METADATA ====== #
-                                dcc.Tab(
-                                    label='Metadata', 
-                                    children=[
-                                        html.Div([
-                                            html.Div([
-                                                html.H3('Metadata portion', style={'color':'black'}),
-                                                # dcc.Textarea(
-                                                #     id='all-terms',
-                                                #     value = 'Fill this in',
-                                                #     readOnly=True,
-                                                #     className='graph-size',
-                                                # ),
-                                            ]),
-                                        ], className="f-h-scroll-div"),
-                                    ],  
-                                    className='cattabs',
-                                    selected_style=selectedTabStyle
-                                ),
-
-                            ]),
-                    ], className="r tab-panel"),
-                ], className="options-right"),
-        ], className='mid'),
+    # === PAGE BODY === #
+    html.Div(
+        id='page-body',
+        children=[
         
-], className="main-div")
+        # ----- LEFT PANEL ----- #
+        html.Div(
+            id='left-panel',
+            children=[
+
+                # --- COLLAPSE BUTTON --- #
+                html.Div(
+                    id='collapse-btn-container',
+                    children=[
+                        html.Button(
+                            id='collapse-btn',
+                            children=[
+                                html.I(className='fa-solid fa-angle-left')
+                            ]
+                        )
+                    ]
+                ),
+
+                # --- SIDE MENU --- #
+                html.Div(
+                    id='side-menu',
+                    className='side-menu-default',
+                    children=[
+                        html.Div(
+                            id='side-menu-content',
+                            children=[
+
+                                # TITLE #
+                                html.Div('Species', id='side-menu-title'),
+
+                                html.Hr(className='separator-line'),
+
+                                # OPTION BUTTONS #
+                                html.Div(
+                                    id='side-menu-options',
+                                    children=[
+                                        html.Button('Cattle', id='cattle-btn', className='options-btn'),
+                                        html.Button('Camels', id='camels-btn', className='options-btn'),
+                                        html.Button('Goats', id='goats-btn', className='options-btn'),
+                                        html.Button('Sheep', id='sheep-btn', className='options-btn'),
+                                    ]
+                                )
+
+                            ]
+                        ),
+                ]),
 
 
-# ******************* CALLBACKS *******************
+            ]
+        ),
 
-# Sidebar Callbacks
-# Callback to update the sidebar.
+        # ----- RIGHT PANEL ----- #
+        html.Div(
+            id='right-panel',
+            children=[
+
+                # --- TABS --- #
+                dbc.Tabs(
+                    children=[
+
+                        # === REPORTS === #
+                        dbc.Tab(
+                            label='Reports',
+                            label_class_name='tab-label',
+                            tab_class_name='tab',
+                            active_label_style=activeLabelStyle,
+                            class_name='tab-content',
+                            children=[
+                                html.Div('Report stuff')
+                                # html.Div(
+                                #     class_name='tab-content',   
+                                #     children=['Report stuff']
+                                # )
+                            ],
+                        ),
+
+                        # === METADATA === #
+                        dbc.Tab(
+                            label='Metadata',
+                            label_class_name='tab-label',
+                            tab_class_name='tab',
+                            active_label_style=activeLabelStyle,
+                            class_name='tab-content',
+                            children=[
+                                html.Div('Metadata stuff')
+                            ],
+                        )
+                ])
+            ]
+        ),
+
+
+        
+        ]
+    ),
+
+], id='dashboard-page')
+
+
+# CALLBACKS #
+
+# COLLAPSE BUTTON STYLE CHANGE #
 @app.callback(
-    Output('left-button', 'children'), 
-    Input('left-button', 'n_clicks')
+    Output('collapse-btn', 'children'),
+    Output('side-menu', 'className'),
+    Input('collapse-btn', 'n_clicks'),
 )
 def change_button_icon(n_clicks):
+    if  n_clicks is None:
+        raise PreventUpdate
+
     if n_clicks%2 == 1:
-        return html.I(className="fa-solid fa-angle-right")
+        return [
+            html.I(className='fa-solid fa-angle-right'),
+            'side-menu-closed',
+            ]
     else:
-        return html.I(className="fa-solid fa-angle-left")
-
-@app.callback(
-    Output('right-div', 'className'), 
-    Input('left-button', 'n_clicks')
-)
-def extend_div(n_clicks):
-    if n_clicks%2 == 1:
-        return "options-right-open"
-    else:
-        return "options-right"
-@app.callback(
-    Output('left-div', 'className'), 
-    Input('left-button', 'n_clicks')
-)
-def slide_div_in(n_clicks):
-    if n_clicks%2 == 1:
-        return "options-left-closed"
-    else:
-        return "options-left"
+        return [
+            html.I(className='fa-solid fa-angle-left'),
+            'side-menu-default',
+        ]
 
 
-# Callback to handle changing the page based on the pathname provided.
-@app.callback(
-    Output('page-content', 'children'),
-    Input('url', 'pathname')
-)
-def display_page(pathname):
-    if pathname == '/dash/':
-        layout = page_1
-    else:
-        layout = "404"
-    return layout
-
-# ===== Run app =====
+# RUN APP #
 if __name__ == '__main__':
-    # app.run_server(use_reloader=True, debug=False, dev_tools_ui=False, dev_tools_props_check=False, host="0.0.0.0", port="8050")
+    # app.run_server(use_reloader=True, debug=False, dev_tools_ui=False, dev_tools_props_check=False, host="0.0.0.0",     port="8050")
     app.run_server(use_reloader=True, debug=True, dev_tools_ui=True, dev_tools_props_check=True, host="0.0.0.0", port="8050")
